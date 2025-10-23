70f9b19cd540b57751904cbd336f0d533b7ea6af
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2025-10-24 02:33:39 +0900

    Set up contollers, models and routes to server

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,22 @@
 import express from 'express'
 import dotenv from 'dotenv'
 
+import { connectDB } from './lib/db.js'
+
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT
 
+app.use(express.json())
+
 app.use('/api/users', userRoutes)
+app.use('/api/auth', authRoutes)
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`)
+	connectDB()
 })
