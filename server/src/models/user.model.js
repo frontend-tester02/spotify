5a0c1e8e511816cd592334384e2998c6c980d056
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2025-10-24 23:46:25 +0900

    User, Stat routes and controlller

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -19,6 +19,4 @@ const userSchema = new mongoose.Schema(
 	{ timestamps: true } //  createdAt, updatedAt
 )
 
-const User = mongoose.model('User', userSchema)
-
-export default User
+export const User = mongoose.model('User', userSchema)
