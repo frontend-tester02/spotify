8b00215c024c7bc411fc0989a77edc5ec9b693e6
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2025-10-24 14:59:12 +0900

    Set up middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
+import { clerkMiddleware } from '@clerk/express'
 import { connectDB } from './lib/db.js'
 
 import userRoutes from './routes/user.route.js'
@@ -12,6 +12,8 @@ const app = express()
 const PORT = process.env.PORT
 
 app.use(express.json())
+
+app.use(clerkMiddleware())
 
 app.use('/api/users', userRoutes)
 app.use('/api/auth', authRoutes)
