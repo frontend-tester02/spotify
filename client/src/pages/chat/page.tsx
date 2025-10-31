e63065077416c250fffa7907567503299ef797c6
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2025-11-01 02:34:49 +0900

    Implementing Socket.io for Realtime Features

--- client/src/pages/chat/page.tsx
+++ client/src/pages/chat/page.tsx
@@ -1,5 +1,32 @@
+import TopBar from '@/components/shared/top-bar'
+import { useChatStore } from '@/hooks/use-chat-store'
+import { useUser } from '@clerk/clerk-react'
+import { useEffect } from 'react'
+import UsersList from './components/users-list'
+
 const ChatPage = () => {
-	return <div>ChatPage</div>
+	const { user } = useUser()
+	const { fetchMessages, fetchUsers, selectedUser, messages } = useChatStore()
+
+	useEffect(() => {
+		if (user) {
+			fetchUsers()
+		}
+	}, [fetchUsers, user])
+
+	useEffect(() => {
+		if (selectedUser) fetchMessages(selectedUser.clerkId)
+	}, [fetchMessages, selectedUser])
+
+	return (
+		<main className='h-full rounded-lg bg-linear-to-b from-zinc-800 to-zinc-900 overflow-hidden'>
+			<TopBar />
+
+			<div className='grid lg:grid-cols-[300px_1fr] grid-cols-[80px_1fr] h-[calc(100vh-180px)]'>
+				<UsersList />
+			</div>
+		</main>
+	)
 }
 
 export default ChatPage
--- client/src/providers/auth.provider.tsx
+++ client/src/providers/auth.provider.tsx
@@ -1,4 +1,5 @@
 import { useAuthStore } from '@/hooks/use-auth-store'
+import { useChatStore } from '@/hooks/use-chat-store'
 import { axiosInstance } from '@/lib/axios'
 import { useAuth } from '@clerk/clerk-react'
 import { Loader } from 'lucide-react'
@@ -13,9 +14,10 @@ const updateApiToken = (token: string | null) => {
 }
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-	const { getToken } = useAuth()
+	const { getToken, userId } = useAuth()
 	const [loading, setLoading] = useState(true)
 	const { checkAdminStatus } = useAuthStore()
+	const { initSocket, disconnectSocket } = useChatStore()
 
 	useEffect(() => {
 		const initAuth = async () => {
@@ -24,6 +26,10 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 				updateApiToken(token)
 				if (token) {
 					await checkAdminStatus()
+					// init socket
+					if (userId) {
+						initSocket(userId)
+					}
 				}
 			} catch (error) {
 				updateApiToken(null)
@@ -33,7 +39,10 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 			}
 		}
 		initAuth()
-	}, [getToken, checkAdminStatus])
+
+		// clean up
+		return () => disconnectSocket()
+	}, [getToken, userId, checkAdminStatus, initSocket, disconnectSocket])
 
 	if (loading)
 		return (
