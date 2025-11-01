de62f043716e424ddbc4cdf2e490c0d53d9ddeb7
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2025-11-01 23:36:54 +0900

    Use node-cron to delete tmp file every one hour

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,8 @@ import fileUpload from 'express-fileupload'
 import cors from 'cors'
 import { createServer } from 'http'
 import path from 'path'
+import fs from 'fs'
+import cron from 'node-cron'
 
 import { connectDB } from './lib/db.js'
 import userRoutes from './routes/user.route.js'
@@ -44,12 +46,35 @@ app.use(
 		},
 	})
 )
+
+// cron jobs
+const tempDir = path.join(process.cwd(), 'tmp')
+cron.schedule('0 * * * *', () => {
+	if (fs.existsSync(tempDir)) {
+		fs.readdir(tempDir, (err, files) => {
+			if (err) {
+				console.log('error', err)
+				return
+			}
+			for (const file of files) {
+				fs.unlink(path.join(tempDir, file), err => {})
+			}
+		})
+	}
+})
 app.use('/api/users', userRoutes)
 app.use('/api/admin', adminRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/songs', songRoutes)
 app.use('/api/albums', albumRoutes)
 app.use('/api/stats', statRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/dist')))
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'))
+	})
+}
 
 app.use((err, req, res, next) => {
 	res.status(500).json({
