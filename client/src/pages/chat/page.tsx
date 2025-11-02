d9b73784f9330c4ddc3420e949f31d5deabac9d4
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2025-11-03 01:38:38 +0900

    Fix chat page message error

--- client/src/pages/chat/page.tsx
+++ client/src/pages/chat/page.tsx
@@ -21,8 +21,6 @@ const ChatPage = () => {
 		if (selectedUser) fetchMessages(selectedUser.clerkId)
 	}, [selectedUser, fetchMessages])
 
-	console.log({ messages })
-
 	return (
 		<main className='h-full rounded-lg bg-linear-to-b from-zinc-800 to-zinc-900 overflow-hidden'>
 			<TopBar />
@@ -39,35 +37,41 @@ const ChatPage = () => {
 							{/* Messages */}
 							<ScrollArea className='h-[calc(100vh-340px)]'>
 								<div className='p-4 space-y-4'>
-									{messages.map(message => (
-										<div
-											key={message._id}
-											className={`flex items-start gap-3 ${
-												message.senderId === user?.id ? 'flex-row-reverse' : ''
-											}`}
-										>
-											<Avatar className='size-8'>
-												<AvatarImage
-													src={
-														message.senderId === user?.id
-															? user.imageUrl
-															: selectedUser.imageUrl
-													}
-												/>
-											</Avatar>
-
+									{Array.isArray(messages) && messages.length > 0 ? (
+										messages.map(message => (
 											<div
-												className={`rounded-lg p-3 max-w-[70%]
+												key={message._id}
+												className={`flex items-start gap-3 ${
+													message.senderId === user?.id
+														? 'flex-row-reverse'
+														: ''
+												}`}
+											>
+												<Avatar className='size-8'>
+													<AvatarImage
+														src={
+															message.senderId === user?.id
+																? user.imageUrl
+																: selectedUser.imageUrl
+														}
+													/>
+												</Avatar>
+
+												<div
+													className={`rounded-lg p-3 max-w-[70%]
 													${message.senderId === user?.id ? 'bg-green-500' : 'bg-zinc-800'}
 												`}
-											>
-												<p className='text-sm'>{message.content}</p>
-												<span className='text-xs text-zinc-300 mt-1 block'>
-													{formatChatTime(message.createdAt)}
-												</span>
+												>
+													<p className='text-sm'>{message.content}</p>
+													<span className='text-xs text-zinc-300 mt-1 block'>
+														{formatChatTime(message.createdAt)}
+													</span>
+												</div>
 											</div>
-										</div>
-									))}
+										))
+									) : (
+										<p className='text-center text-zinc-400'>No messages yet</p>
+									)}
 								</div>
 							</ScrollArea>
 
